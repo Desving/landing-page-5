401032393ab4a9753cccf621c7e9320cc7601efd
Desving <31211259+Desving@users.noreply.github.com>
2017-09-20 18:48:30 +0300

    animation-2

---
+++ js/common.js
@@ -0,0 +1,10 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    $("#ani-zoom").animated("flipInY", "fadeOut");
+    $("#ani-zoom1").animated("flipInY", "fadeOut");
+    $("#ani-zoom2").animated("flipInY", "fadeOut");
+    $("#ani-left").animated("bounceInLeft", "fadeOut");
+    $("#ani-down").animated("bounceInDown", "fadeOut");
+});
